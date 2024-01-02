ac64d3e4c262a68806af6675f9e498673e5289ed
nm1008 <mallari08nikko@yahoo.com>
2024-01-02 20:49:42 +0800

    backend was deployed, currently fixing cors issue

--- server.js
+++ server.js
@@ -10,14 +10,29 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://capstone-mern-front.vercel.app/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://capstone-mern-front.vercel.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
+app.use(cors(corsOptions));
 
-app.options('*', cors())
+app.options("*", cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //PORT
 const PORT = process.env.PORT || 8000;
